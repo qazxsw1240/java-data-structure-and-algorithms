804d3826c9cbc8a8ba048370975ee85b1176aec9
qazxsw1240 <qazxsw010528@gmail.com>
2024-01-30 06:28:33 +0900

    update Set

--- src/java/src/algo/Set.java
+++ src/java/src/algo/Set.java
@@ -1,15 +1,15 @@
 package src.algo;
 
 public interface Set<E> extends DataStructure {
-    public void add(E e);
+    public abstract void add(E e);
 
-    public boolean remove(E e);
+    public abstract boolean remove(E e);
 
-    public boolean contains(E e);
+    public abstract boolean contains(E e);
 
-    public Set<E> union(Set<E> s);
+    public abstract Set<E> union(Set<E> s);
 
-    public Set<E> intersection(Set<E> s);
+    public abstract Set<E> intersection(Set<E> s);
 
-    public Set<E> difference(Set<E> s);
+    public abstract Set<E> difference(Set<E> s);
 }
